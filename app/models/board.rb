f7924f7aaa88c1d1fc0d2fc9b1e75465deb23954
Yannick Dulous <yannick@fantomass.com>
2016-08-17 16:35:06 +0200

    Creation de boards_controller.rb

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,19 +4,19 @@ class Board < ApplicationRecord
 
   validates :name, presence: true
 
-  validates :category, presence: true
+  # validates :category, presence: true
 
   validates :size, presence: true
 
   validates :price_per_day, presence: true
 
-  validates :description, presence: true
+  # validates :description, presence: true
 
   validates :address, presence: true
 
-  validates :city, presence: true
+  # validates :city, presence: true
 
-  validates :country, presence: true
+  # validates :country, presence: true
 
   validates :automatic_reservation, presence: true
 
