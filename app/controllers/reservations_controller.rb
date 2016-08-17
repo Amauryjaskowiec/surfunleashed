d97cd4978cad1009a7d2ab3e8c9db7083d77931a
Yannick Dulous <yannick@fantomass.com>
2016-08-17 18:45:59 +0200

    login signin user

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,35 @@
 class ReservationsController < ApplicationController
+
+   before_action :find_reservation, only: [:show, :update]
+
   def new
+    @reservation = Reservation.new
+  end
+
+  def show
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.save
   end
 
   def update
+    @reservation.update(reservation_params)
   end
 
   def destroy
   end
+
+private
+
+  def find_reservation
+    @reservation = Reservation.find(:id)
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :surfer[:id], :board[id])
+  end
+
 end
+
