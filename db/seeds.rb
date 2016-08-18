988a3237ca7743bc168fd2fb17558dbf6818836e
Yannick Dulous <yannick@fantomass.com>
2016-08-18 15:26:26 +0200

    fixcategory seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+Category.destroy_all
 shortboard = Category.create(name: "Shortboard")
 longboard = Category.create(name: "Longboard")
 funboard = Category.create(name: "Funboard")
