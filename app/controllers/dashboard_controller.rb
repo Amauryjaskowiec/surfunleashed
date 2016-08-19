9f0498764d5b404cec6057c8a9450eabc43eab97
amauryjaskowiec <amaury.jaskowiec@gmail.com>
2016-08-19 17:52:31 +0200

    dashboard controller fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,13 @@
 class DashboardController < ApplicationController
- def myboards
-   @myboards = board.user
-   puts @myboards
- end
+  def myboards
+    @myboards = board.user
+    puts @myboards
+  end
 
- def destroy
-   @board = Board.find(params[:id])
-   @board.destroy
- end
-
- def myreservation
-
- end
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+  end
+  def myreservation
+  end
 end
